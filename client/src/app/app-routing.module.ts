8773bc3cab34668aaab76b59912baad13de80feb
ola awaly <awaly.ola@gmail.com>
2024-09-28 08:07:12 +0200

    modifier les routes ,garder le dashboard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,15 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path:'', redirectTo:'dashboard',pathMatch:'full'},
-  {path:'modeles',loadChildren:()=> import('./modeles/modeles.module').then(c => c.ModelesModule) },
-  {path:'cv',loadChildren:()=> import('./cv/cv.module').then(c => c.CvModule) },
-  {path:'users',loadChildren:()=>import('./users/users.module').then(c => c.UsersModule)},
-  {path:'dashboard',component:DashboardComponent}
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  //  {path:'modeles',loadChildren:()=> import('./modeles/modeles.module').then(c => c.ModelesModule) },
+  // {path:'cv',loadChildren:()=> import('./cv/cv.module').then(c => c.CvModule) },
+  // {path:'users',loadChildren:()=>import('./users/users.module').then(c => c.UsersModule)},
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
