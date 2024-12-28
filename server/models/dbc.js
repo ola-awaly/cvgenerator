85b4ef98abca9725d6fda1865bd09c8c13e4f7ec
ola awaly <awaly.ola@gmail.com>
2024-12-28 23:16:23 +0100

    console log to debug

--- server/models/dbc.js
+++ server/models/dbc.js
@@ -10,6 +10,18 @@ const lienModel = require('./liens.model');
 const experienceModel = require('./experiences.model');
 const formationModel = require('./formations.model');
 let dbConnector;
+console.log(
+	'NAME_DB:' +
+		process.env.NAME_DB +
+		'USER_DB:' +
+		process.env.USER_DB +
+		'PASSWORD_DB:' +
+		process.env.PASSWORD_DB +
+		'HOST_DB:' +
+		process.env.HOST_DB +
+		'PORT_DB' +
+		process.env.PORT_DB
+);
 
 module.exports = {
 	connect: () => {
